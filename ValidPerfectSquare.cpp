21189199f978f50f012ebd7a94be3cc65c6b4292
singhshreya <36243674+singhshreya@users.noreply.github.com>
2020-05-09 20:32:59 +0530

    Create ValidPerfectSquare.cpp
      Valid Perfect Square
    Solution
    Given a positive integer num, write a function which returns True if num is a perfect square else False.
    
    Note: Do not use any built-in library function such as sqrt.
    
    Example 1:
    
    Input: 16
    Output: true
    Example 2:
    
    Input: 14
    Output: false

---
+++ ValidPerfectSquare.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) 
+    {
+        unsigned int i=1;
+        for(i=1;i*i<=num;i++)
+        {
+            if(num/i==i && num%i==0)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/// We used unsigned int here as simple [read signed] int gave overflow error.
